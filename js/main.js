a9e66346c12e03be7f697d895a1d37dd5566a27d
[Valentina] <[valentina.giffi@ad2014.it]>
2015-07-13 12:33:09 +0200

    controller

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-var listaDellaSpesa = angular.module('listApp', []);
+var listaDellaSpesa = angular.module('listApp', ['ngRoute']);
+
+listaDellaSpesa.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/index', {
+                templateUrl: 'index'
+            }).
+            when('/todo', {
+                templateUrl: 'todo'
+            }).
+            when('/comprate', {
+                templateUrl: 'comprate',
+                controller: 'comprateCtrl'
+            }).
+            otherwise({
+                redirectTo: '/index'
+            });
+    }]);
+
+
 listaDellaSpesa.controller('ListCtrl', ['$scope',function ListCtrl($scope) {
   $scope.lista = [
     {
@@ -17,3 +37,15 @@ listaDellaSpesa.controller('ListCtrl', ['$scope',function ListCtrl($scope) {
     });
   };
 }]);
+
+    listaDellaSpesa.controller('comprateCtrl', ['$scope',function comprateCtrl($scope) {
+      $scope.cancella = function(){
+        var arrayLength = $scope.$parent.lista.length;
+        for(var i = 0;i < arrayLength;i++){
+            if($scope.$parent.lista[i].comprato)
+            {
+                $scope.$parent.lista.splice(i,1);
+            }
+        }
+      }
+    }]);
